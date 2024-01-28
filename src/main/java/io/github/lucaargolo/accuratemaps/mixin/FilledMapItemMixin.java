3ec44e1d1b8b394f290ce7e7fd40785de0deea55
giacomo (desktop) <157554814+giacomogarau@users.noreply.github.com>
2024-01-28 19:19:24 +0100

    mixin update
    Co-authored-by: R2bEEaton <scgryan@gmail.com>

--- src/main/java/io/github/lucaargolo/accuratemaps/mixin/FilledMapItemMixin.java
+++ src/main/java/io/github/lucaargolo/accuratemaps/mixin/FilledMapItemMixin.java
@@ -8,7 +8,9 @@ import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.PersistentState;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,12 +30,12 @@ public abstract class FilledMapItemMixin {
     public void captureAccurateMapState(ItemStack stack, World world, Entity entity, int slot, boolean selected, CallbackInfo ci) {
         Integer id = FilledMapItem.getMapId(stack);
         if(id != null && world instanceof ServerWorld serverWorld) {
-            currentState = serverWorld.getServer().getOverworld().getPersistentStateManager().getOrCreate(AccurateMapState.Companion::createFromNbt, AccurateMapState::new, "accurate_map_"+id);
+            currentState = serverWorld.getServer().getOverworld().getPersistentStateManager().getOrCreate(new PersistentState.Type<>(AccurateMapState::new,AccurateMapState.Companion::createFromNbt,null), "accurate_map_"+id);
         }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;putColor(IIB)Z"), method = "updateColors", locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void updateAccurateMapState(World world, Entity entity, MapState state, CallbackInfo ci, int i, int j, int k, int l, int m, int n, MapState.PlayerUpdateTracker playerUpdateTracker, boolean bl, int o, double d, int p, int q, int r, boolean bl2, int s, int t, Multiset<MapColor> multiset, WorldChunk worldChunk, ChunkPos chunkPos, int u, int v, int w, double e, MapColor mapColor, MapColor.Brightness brightness) {
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;putColor(IIB)Z"), method = "updateColors", locals = LocalCapture.CAPTURE_FAILHARD)
+    public void updateAccurateMapState(World world, Entity entity, MapState state, CallbackInfo ci, int i, int j, int k, int l, int m, int n, MapState.PlayerUpdateTracker playerUpdateTracker, BlockPos.Mutable pos1, BlockPos.Mutable pos2, boolean bl, int o, double d, int p, int r, boolean bl2, int s, int t, Multiset<MapColor> multiset, WorldChunk worldChunk, int u, double e, MapColor mapColor, MapColor.Brightness brightness) {
         currentState.setBlock(o, p, mapColor);
     }
 
